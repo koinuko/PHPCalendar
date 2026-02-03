60a853def444048b2be496b2fd1e074c4381f50b
koinuko <koinuko@yahoo.com>
2026-02-03 15:16:20 -0700

    Create example.php

---
+++ example.php
@@ -0,0 +1,16 @@
+<head>
+  <title>My Calendar</title>
+  <link rel="stylesheet" href="style.css"/>
+</head>
+<html>
+<body>
+
+  <?php
+    include "events.php";
+    include "calendarbuild.php";
+  ?>
+  
+</body>
+
+  
+</html>
